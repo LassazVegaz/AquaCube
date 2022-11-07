8ff0dc58c509c183096d10369cfa655f9a22016d
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-08 04:31:33 +0530

    GLA <- CGA

--- app/src/main/java/com/example/icecube/activities/goals/CreateGoalActivity.java
+++ app/src/main/java/com/example/icecube/activities/goals/CreateGoalActivity.java
@@ -3,6 +3,8 @@ package com.example.icecube.activities.goals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.icecube.R;
 
@@ -12,5 +14,12 @@ public class CreateGoalActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_goal);
+
+        ImageView backImg = (ImageView) findViewById(R.id.create_goal_back_icon);
+        backImg.setOnClickListener((v) -> onBackIconClicked());
+    }
+
+    private void onBackIconClicked() {
+        finish();
     }
 }
