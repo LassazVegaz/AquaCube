7bbb15dcff80fb9e5a38a5560b7860f1b44a6f8e
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-08 04:23:19 +0530

    GLA -> CGA

--- app/src/main/java/com/example/icecube/activities/goals/GoalsListActivity.java
+++ app/src/main/java/com/example/icecube/activities/goals/GoalsListActivity.java
@@ -2,7 +2,11 @@ package com.example.icecube.activities.goals;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.icecube.R;
 
@@ -12,5 +16,14 @@ public class GoalsListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goals_list);
+
+        Button addBtn = (Button) findViewById(R.id.goals_list_add_btn);
+        addBtn.setOnClickListener((v) -> onAddButtonClick());
     }
+
+    private void onAddButtonClick() {
+        Intent i = new Intent(this, CreateGoalActivity.class);
+        startActivity(i);
+    }
+
 }
