1c4c7a455542e75994fb4866bbadd6f8adbedabb
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-07 13:19:31 +0530

    create plan act

---
+++ app/src/main/java/com/example/icecube/activites/goals/CreatePlanActivity.java
@@ -0,0 +1,16 @@
+package com.example.icecube.activites.goals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.icecube.R;
+
+public class CreatePlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_plan);
+    }
+}
