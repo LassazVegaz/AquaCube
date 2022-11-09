0aa1ff870e9e5ef0dab01b23c4a771ce0dcb6fcb
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-09 16:57:17 +0530

    something

--- app/src/main/java/com/example/icecube/activities/goals/CreatePlanActivity.java
+++ app/src/main/java/com/example/icecube/activities/goals/CreatePlanActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.icecube.R;
 
@@ -13,5 +14,13 @@ public class CreatePlanActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_plan);
+
+        findViewById(R.id.create_plan_add_reminder_btn).setOnClickListener(this::onCreateReminderClicked);
+    }
+
+
+    // events
+    void onCreateReminderClicked(View v) {
+
     }
 }
