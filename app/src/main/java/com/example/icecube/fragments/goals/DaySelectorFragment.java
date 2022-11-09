d84a88a3eb6741af6e3ef707d8c79516d4462999
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-09 21:47:32 +0530

    on off days

--- app/src/main/java/com/example/icecube/fragments/goals/DaySelectorFragment.java
+++ app/src/main/java/com/example/icecube/fragments/goals/DaySelectorFragment.java
@@ -1,5 +1,7 @@
 package com.example.icecube.fragments.goals;
 
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -10,40 +12,16 @@ import android.view.ViewGroup;
 
 import com.example.icecube.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link DaySelectorFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class DaySelectorFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    int onColor, offColor;
 
     public DaySelectorFragment() {
-        // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment DaySelectorFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static DaySelectorFragment newInstance(String param1, String param2) {
+    public static DaySelectorFragment newInstance() {
         DaySelectorFragment fragment = new DaySelectorFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -52,15 +30,33 @@ public class DaySelectorFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+            // store args in vars
         }
+        onColor = getResources().getColor(R.color.day_selector_selected);
+        offColor = getResources().getColor(R.color.day_selector_unselected);
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_day_selector, container, false);
+        View view = inflater.inflate(R.layout.fragment_day_selector, container, false);
+
+        view.findViewById(R.id.day_selector_mo).setOnClickListener(this::onDayButtonClick);
+        view.findViewById(R.id.day_selector_tu).setOnClickListener(this::onDayButtonClick);
+        view.findViewById(R.id.day_selector_we).setOnClickListener(this::onDayButtonClick);
+        view.findViewById(R.id.day_selector_th).setOnClickListener(this::onDayButtonClick);
+        view.findViewById(R.id.day_selector_fr).setOnClickListener(this::onDayButtonClick);
+        view.findViewById(R.id.day_selector_sa).setOnClickListener(this::onDayButtonClick);
+        view.findViewById(R.id.day_selector_su).setOnClickListener(this::onDayButtonClick);
+
+        return view;
+    }
+
+
+    // events
+    void onDayButtonClick(View v) {
+        ColorDrawable bg = (ColorDrawable) v.getBackground();
+        if (bg.getColor() == onColor) v.setBackgroundColor(offColor);
+        else v.setBackgroundColor(onColor);
     }
 }
