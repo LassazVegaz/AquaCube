410320ed389cba7deb1f7399eaba6676d1630252
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-11 00:17:40 +0530

    auth service complete

--- app/src/main/java/com/example/icecube/services/AuthService.java
+++ app/src/main/java/com/example/icecube/services/AuthService.java
@@ -1,9 +1,29 @@
 package com.example.icecube.services;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class AuthService {
-    final String uid = "QYoIei5SddX9mCH86t9B";
+    final FirebaseAuth fa = FirebaseAuth.getInstance();
 
     public String getLoggedUserId() {
-        return uid;
+        return fa.getCurrentUser().getUid();
+    }
+
+    public boolean isLoggedIn() {
+        return getLoggedUserId() != null;
+    }
+
+    public void login(String email, String password, OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener) {
+        fa.signInWithEmailAndPassword(email, password)
+                .addOnSuccessListener(authResult -> onSuccessListener.onSuccess(null))
+                .addOnFailureListener(onFailureListener);
+    }
+
+    public void register(String email, String password, OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener) {
+        fa.createUserWithEmailAndPassword(email, password)
+                .addOnSuccessListener(res -> onSuccessListener.onSuccess(null))
+                .addOnFailureListener(onFailureListener);
     }
 }
