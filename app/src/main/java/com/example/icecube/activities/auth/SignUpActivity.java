8c37eda285fe53af6439028a4bb39cb16fe6b747
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-11 01:23:12 +0530

    auth is done

--- app/src/main/java/com/example/icecube/activities/auth/SignUpActivity.java
+++ app/src/main/java/com/example/icecube/activities/auth/SignUpActivity.java
@@ -6,17 +6,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.example.icecube.R;
+import com.example.icecube.activities.HomeActivity;
+import com.example.icecube.services.AuthService;
+import com.example.icecube.services.ServiceLocator;
 
 public class SignUpActivity extends AppCompatActivity {
+    final AuthService as = ServiceLocator.getInstance().getAuthService();
+
     EditText emailTxt, passwordTxt;
+    FrameLayout spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        spinner = findViewById(R.id.signup_spinner);
         emailTxt = findViewById(R.id.signup_email_txt);
         passwordTxt = findViewById(R.id.signup_password_txt);
 
@@ -26,8 +35,30 @@ public class SignUpActivity extends AppCompatActivity {
 
     void onLoginClick(View v) {
         Intent i = new Intent(this, SignInActivity.class);
+        finish();
         startActivity(i);
     }
 
-    void onRegisterClick(View v) {}
+    void onRegisterClick(View v) {
+        showSpinner();
+        as.register(emailTxt.getText().toString(), passwordTxt.getText().toString(),
+                unused -> {
+                    hideSpinner();
+                    Intent i = new Intent(this, HomeActivity.class);
+                    finish();
+                    startActivity(i);
+                }, e -> {
+                    hideSpinner();
+                    Toast.makeText(this, "This email address already has an account", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+
+    void showSpinner() {
+        spinner.setVisibility(View.VISIBLE);
+    }
+
+    void hideSpinner() {
+        spinner.setVisibility(View.GONE);
+    }
 }
--- app/src/main/java/com/example/icecube/services/AuthService.java
+++ app/src/main/java/com/example/icecube/services/AuthService.java
@@ -3,16 +3,18 @@ package com.example.icecube.services;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
 
 public class AuthService {
     final FirebaseAuth fa = FirebaseAuth.getInstance();
+    final FirebaseFirestore ff = FirebaseFirestore.getInstance();
+    final FirebaseDatabase fd = FirebaseDatabase.getInstance();
 
     public String getLoggedUserId() {
         return fa.getCurrentUser().getUid();
-    }
-
-    public boolean isLoggedIn() {
-        return getLoggedUserId() != null;
     }
 
     public void login(String email, String password, OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener) {
@@ -23,7 +25,21 @@ public class AuthService {
 
     public void register(String email, String password, OnSuccessListener<Void> onSuccessListener, OnFailureListener onFailureListener) {
         fa.createUserWithEmailAndPassword(email, password)
-                .addOnSuccessListener(res -> onSuccessListener.onSuccess(null))
+                .addOnSuccessListener(res -> {
+                    ff.collection("users")
+                            .document(res.getUser().getUid())
+                            .set(new HashMap<String, Object>() {{
+                                put("email", res.getUser().getEmail());
+                            }})
+                            .addOnSuccessListener(d -> {
+                                fd.getReference()
+                                        .child("users")
+                                        .child(res.getUser().getUid())
+                                        .child("email")
+                                        .setValue(res.getUser().getEmail())
+                                        .addOnSuccessListener(unused -> onSuccessListener.onSuccess(null));
+                            });
+                })
                 .addOnFailureListener(onFailureListener);
     }
 }
