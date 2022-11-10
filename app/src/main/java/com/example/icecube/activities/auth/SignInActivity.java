b7bb161e215c13b92805de895704d756eb688981
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-10 22:41:49 +0530

    auth basics

--- app/src/main/java/com/example/icecube/activities/auth/SignInActivity.java
+++ app/src/main/java/com/example/icecube/activities/auth/SignInActivity.java
@@ -2,15 +2,33 @@ package com.example.icecube.activities.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.icecube.R;
 
 public class SignInActivity extends AppCompatActivity {
+    EditText emailTxt, passwordTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+
+        emailTxt = findViewById(R.id.signin_email_txt);
+        passwordTxt = findViewById(R.id.signin_password_txt);
+
+        findViewById(R.id.signin_login_btn).setOnClickListener(this::onLoginClick);
+        findViewById(R.id.signin_register_btn).setOnClickListener(this::onRegisterClick);
+    }
+
+    void onLoginClick(View v) {
+    }
+
+    void onRegisterClick(View v) {
+        Intent i = new Intent(this, SignUpActivity.class);
+        startActivity(i);
     }
 }
--- app/src/main/java/com/example/icecube/activities/auth/SignUpActivity.java
+++ app/src/main/java/com/example/icecube/activities/auth/SignUpActivity.java
@@ -2,15 +2,32 @@ package com.example.icecube.activities.auth;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.example.icecube.R;
 
 public class SignUpActivity extends AppCompatActivity {
+    EditText emailTxt, passwordTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+        emailTxt = findViewById(R.id.signup_email_txt);
+        passwordTxt = findViewById(R.id.signup_password_txt);
+
+        findViewById(R.id.signup_login_btn).setOnClickListener(this::onLoginClick);
+        findViewById(R.id.signup_register_btn).setOnClickListener(this::onRegisterClick);
     }
+
+    void onLoginClick(View v) {
+        Intent i = new Intent(this, SignInActivity.class);
+        startActivity(i);
+    }
+
+    void onRegisterClick(View v) {}
 }
