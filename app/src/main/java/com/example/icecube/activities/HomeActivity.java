f4fbdee403178cf24b7a8d10a9485b24ee735203
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-10 21:53:12 +0530

    H -> GL

--- app/src/main/java/com/example/icecube/activities/HomeActivity.java
+++ app/src/main/java/com/example/icecube/activities/HomeActivity.java
@@ -2,9 +2,12 @@ package com.example.icecube.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.icecube.R;
+import com.example.icecube.activities.goals.GoalsListActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -12,5 +15,13 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        findViewById(R.id.home_goals_menu).setOnClickListener(this::onGoalsMenuClick);
     }
+
+    void onGoalsMenuClick(View v) {
+        Intent i = new Intent(this, GoalsListActivity.class);
+        startActivity(i);
+    }
+
 }
