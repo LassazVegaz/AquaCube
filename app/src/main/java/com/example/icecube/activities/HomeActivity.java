8c89ea001b07a8e4de259e024da3fe04970bcb68
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-10 20:23:38 +0530

    empty acts

---
+++ app/src/main/java/com/example/icecube/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.example.icecube.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.icecube.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
