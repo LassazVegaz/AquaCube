25e53ff0ae4dc3b294198d8c190359e84a3be8bf
LassazVegaz <lasindunuwanga@gmail.com>
2022-11-12 16:05:16 +0530

    reminder motivation text

--- app/src/main/java/com/example/icecube/activities/goals/CreateReminderActivity.java
+++ app/src/main/java/com/example/icecube/activities/goals/CreateReminderActivity.java
@@ -3,16 +3,20 @@ package com.example.icecube.activities.goals;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.Switch;
+import android.widget.TextView;
 
 import com.example.icecube.R;
 import com.example.icecube.fragments.HeaderFragment;
 import com.example.icecube.models.Reminder;
 import com.example.icecube.services.ServiceLocator;
+import com.example.icecube.services.goals.PlansService;
 import com.example.icecube.services.goals.RemindersService;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.switchmaterial.SwitchMaterial;
@@ -21,6 +25,10 @@ public class CreateReminderActivity extends AppCompatActivity {
     public static final String
             PARAMS_GOAL_ID = "goalId", PARAMS_PLAN_ID = "planId", PARAMS_REMINDER_ID = "reminderId";
 
+    private final static String
+            MTV_TEXT_BAD = "%d cups are remaining", MTV_EMOJI_BAD = "😬",
+            MTV_TEXT_GOOD = "This is a great plan", MTV_EMOJI_GOOD = "🫡";
+
     RemindersService rs;
 
     String goalId, planId, reminderId;
@@ -28,6 +36,7 @@ public class CreateReminderActivity extends AppCompatActivity {
     SwitchMaterial soundSwitch, vibrationSwitch;
     FrameLayout spinner;
     Button discardBtn;
+    TextView mtvTxt, mtvEmojiTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,9 +45,26 @@ public class CreateReminderActivity extends AppCompatActivity {
 
         spinner = findViewById(R.id.create_rem_spinner);
         timeTxt = findViewById(R.id.create_rem_time_txt);
-        noOfCupsTxt = findViewById(R.id.create_rem_cups_txt);
         soundSwitch = findViewById(R.id.create_rem_sound_switch);
         vibrationSwitch = findViewById(R.id.create_rem_vibration_switch);
+        mtvTxt = findViewById(R.id.create_rem_mtv_txt);
+        mtvEmojiTxt = findViewById(R.id.create_rem_mtv_emoji);
+
+        noOfCupsTxt = findViewById(R.id.create_rem_cups_txt);
+        noOfCupsTxt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                setMotivationText();
+            }
+        });
 
         discardBtn = findViewById(R.id.create_rem_discard_btn);
         discardBtn.setOnClickListener(this::onDiscardClick);
@@ -55,6 +81,8 @@ public class CreateReminderActivity extends AppCompatActivity {
             discardBtn.setText("Delete");
             loadData();
         }
+
+        setMotivationText();
     }
 
 
@@ -126,6 +154,31 @@ public class CreateReminderActivity extends AppCompatActivity {
 
     void hideSpinner() {
         spinner.setVisibility(View.GONE);
+    }
+
+    void setMotivationText() {
+        PlansService ps = ServiceLocator.getInstance().getPlansService(goalId);
+        ps.getRemainingCups(planId, cups -> {
+            String txt = MTV_TEXT_GOOD, emo = MTV_EMOJI_GOOD;
+
+            try {
+                int _cups = Integer.parseInt(noOfCupsTxt.getText().toString());
+                cups = cups - _cups;
+            } catch (Exception e) {
+            }
+
+            if (cups > 0) {
+                txt = String.format(MTV_TEXT_BAD, cups);
+                emo = MTV_EMOJI_BAD;
+            }
+
+            String finalTxt = txt;
+            String finalEmo = emo;
+            runOnUiThread(() -> {
+                mtvTxt.setText(finalTxt);
+                mtvEmojiTxt.setText(finalEmo);
+            });
+        });
     }
 
 }
