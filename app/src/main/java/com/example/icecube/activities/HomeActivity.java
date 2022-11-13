71712213f1be1e08c9ce62233159923e69ed952b
Mudannayake M.A.P.H.N <it20248994@my.sliit.lk>
2022-11-13 18:48:07 +0530

    connected

--- app/src/main/java/com/example/icecube/activities/HomeActivity.java
+++ app/src/main/java/com/example/icecube/activities/HomeActivity.java
@@ -9,6 +9,8 @@ import android.view.View;
 import com.example.icecube.R;
 import com.example.icecube.activities.auth.SignInActivity;
 import com.example.icecube.activities.goals.GoalsListActivity;
+import com.example.icecube.activities.hiruni.ComplaintsList;
+import com.example.icecube.activities.hiruni.ManageSocieties;
 import com.example.icecube.services.AuthService;
 import com.example.icecube.services.ServiceLocator;
 
@@ -22,10 +24,28 @@ public class HomeActivity extends AppCompatActivity {
 
         findViewById(R.id.home_goals_menu).setOnClickListener(this::onGoalsMenuClick);
         findViewById(R.id.home_logout_btn).setOnClickListener(this::onLogoutClick);
+        findViewById(R.id.home_campaigns_menu).setOnClickListener(this::onCampaignsMenuClick);
+        findViewById(R.id.home_complains_menu).setOnClickListener(this::onComlainsMenuClick);
+        findViewById(R.id.home_population_menu).setOnClickListener(this::onPopulationMenuClick);
     }
 
     void onGoalsMenuClick(View v) {
         Intent i = new Intent(this, GoalsListActivity.class);
+        startActivity(i);
+    }
+
+    void onPopulationMenuClick(View v) {
+        Intent i = new Intent(this, ManageSocieties.class);
+        startActivity(i);
+    }
+
+    void onCampaignsMenuClick(View v) {
+        Intent i = new Intent(this, GoalsListActivity.class);
+        startActivity(i);
+    }
+
+    void onComlainsMenuClick(View v) {
+        Intent i = new Intent(this, ComplaintsList.class);
         startActivity(i);
     }
 
